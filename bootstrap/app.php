e7a921fd6f6a48bd836bc38039e95d4b55c18124
Thakvika1 <thakvika.thorn@gmail.com>
2025-12-03 17:49:37 +0700

    migrate table

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Add global middleware here
         $middleware->append(ApiKeyMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
